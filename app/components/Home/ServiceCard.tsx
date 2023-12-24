de3439aff5294fe064e2721144e9d189324584aa
Dhruv Kumar Sharma <sharmaaniket682@gmail.com>
2023-12-24 18:06:55 +0530

    sbi2

--- app/components/Home/ServiceCard.tsx
+++ app/components/Home/ServiceCard.tsx
@@ -1,27 +1,21 @@
 "use client";
 import React from "react";
-import Link from "next/link";
-import { motion } from "framer-motion";
-
+import Link from "next/link"; 
 // SwiperSlider.js
 export const ServiceCard = ({ imageUrl , title, description, link }:any) => {
 
   return (
     <Link href={link}>
-      <motion.div
-        className="bg-gray-200 py-2 rounded-lg shadow-md transition-transform hover:shadow-lg hover:scale-105 hover:bg-gray-100 mb-5"
-        whileHover={{ scale: 1.05, backgroundColor: "#f0f0f0" }}
-        whileTap={{ scale: 50.95, opacity: 0, transition: { duration: 2 } }}
-
-
+      <div
+        className="bg-gray-200 pb-2 rounded-lg shadow-md transition-transform hover:shadow-lg hover:scale-105 focus:scale-[1.02] hover:bg-gray-100 mb-5" 
       >
         <img
           src={imageUrl}
           alt={title}
           className="mb-8 rounded-md w-full h-64 object-cover" />
-        <h3 className="text-2xl font-Gentona uppercase font-semibold text-indigo-800 mb-4">{title}</h3>
-        <p className="text-gray-600 font-Ikaros font-bold sm:p-[27px]">{description}</p>
-      </motion.div>
+        <h3 className="text-2xl font-Gentona uppercase font-semibold text-indigo-800 px-2 mb-4">{title}</h3>
+        <p className="text-gray-600 font-Ikaros font-bold sm:p-[27px] px-2">{description}</p>
+      </div>
     </Link>
   );
 };
