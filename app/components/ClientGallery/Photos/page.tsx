b16cf4be26114db1bdb59e8c81af8373e81cfbe5
Dhruv Kumar Sharma <115400138+Dhruv628@users.noreply.github.com>
2024-01-01 11:07:40 +0530

    Update zindex cg photos page.tsx

--- app/components/ClientGallery/Photos/page.tsx
+++ app/components/ClientGallery/Photos/page.tsx
@@ -31,7 +31,7 @@ const FullScreenImageModal: React.FC<FullScreenImageModalProps> = ({
   onClose,
 }) => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80">
+    <div style={{ zIndex:6100 }} className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80">
       <div className="max-w-3xl max-h-3/4">
         <button
           style={{ zIndex: 6100 }}
